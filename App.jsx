76f9fb326bb8a6e4f0e988ec57c33d4b96447c54
Aman Kumar <117816120+amanknandal@users.noreply.github.com>
2026-05-18 15:46:07 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -16,6 +16,7 @@ const App = () => {
         <Route path="/pdf/:id" element={<PDFViewerPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
